aa1c088e79569784a13446828649277019d2d0f7
Mehidi Hasan <www.mehidi.me@gmail.com>
2024-03-22 15:05:57 +0600

    Mobile Menu added

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,11 @@
+"use client";
 import Link from "next/link";
+import { useState } from "react";
 
 function Header() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => setShowMenu((pre) => !pre);
   return (
     <header>
       <div className="container header">
@@ -9,15 +14,19 @@ function Header() {
             <img src="/images/logo.png" alt="" />
           </Link>
         </div>
-        <nav className="top-nav">
-          <Link href={"/"}>Home</Link>
-          <Link href={"/about"}>About us</Link>
-          <a href="#">Contact</a>
-          <a href="#">Policyholder Portal</a>
-          <button className="btn">Report a Claim</button>
+        <nav className={showMenu ? `top-nav active` : `top-nav`}>
+          <Link onClick={toggleMenu} href={"/"}>Home</Link>
+          <Link onClick={toggleMenu} href={"/about"}>About us</Link>
+          <a onClick={toggleMenu} href="#">Contact</a>
+          <a onClick={toggleMenu} href="#">Policyholder Portal</a>
+          <button onClick={toggleMenu} className="btn">Report a Claim</button>
         </nav>
-        <div className="menu">
-          <img src="/icons/bars-3-bottom-right.svg" alt="" />
+        <div className="menu" onClick={toggleMenu}>
+          {showMenu ? (
+            <img src="/icons/x-mark.svg" alt="" />
+          ) : (
+            <img src="/icons/bars-3-bottom-right.svg" alt="" />
+          )}
         </div>
       </div>
     </header>
